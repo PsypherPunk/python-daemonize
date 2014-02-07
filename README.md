b81986ae0035d8f7fbdee1d6c30680ae3b7f9a24
Roger G. Coram <roger.coram@bl.uk>
2014-02-07 17:56:03 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+python-daemonize
+==============
+
+Taken from http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name="python-daemonize",
+	version="0.0.1",
+	author="Roger G. Coram",
+	author_email="roger.coram@bl.uk",
+	packages=[ "daemonize" ],
+	license="LICENSE.txt",
+	description="Python daemon implementation.",
+	long_description=open( "README.md" ).read(),
+	install_requires=[
+	],
+)
